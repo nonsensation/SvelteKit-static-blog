ad2f262e0a16cfb48c018c6d8a9f3f730b72b6dc
3li <nonsensation@gmail.com>
2026-02-09 13:00:15 +0100

    fixed to use resolve() instead of paths.base

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,7 +1,8 @@
 import type { PostMetadata } from "$lib/types";
+import { resolve } from '$app/paths';
 
 export async function load({ fetch }) {
-    const response = await fetch('/api/posts');
+    const response = await fetch(resolve(`/api/posts`));
     const posts: PostMetadata[] = await response.json();
     return { posts };
 }
